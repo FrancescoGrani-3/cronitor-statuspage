d583edbe1ee50ceb0e77b19d0817bf440953dc00
Vahid Al <thevahidal@gmail.com>
2021-12-15 23:44:16 +0330

    getting rid of cache controls for internal apis

--- pages/api/monitors/[key]/pings.js
+++ pages/api/monitors/[key]/pings.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   const { key } = req.query
   try {
     const { data, status } = await listMonitorPings(key)
-    res.setHeader('Cache-Control', 's-maxage=0, stale-while-revalidate=0')
+    // res.setHeader('Cache-Control', 's-maxage=0, stale-while-revalidate=0')
     res.status(status).json(data)
   } catch (error) {
     const { data, status } = error.response
